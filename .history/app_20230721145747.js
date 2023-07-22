379d21c4179d6d85fa81b64caf4b475520053340
Divyanshu <divyanshukalola88@gmail.com>
2023-07-22 09:51:20 -0500

    added starter ejs pages

---
+++ .history/app_20230721145747.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.json()); // Parse JSON request bodies
+
+// Set the template engine
+app.set("view engine", "ejs");
+app.use("/assets", express.static("public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home", { activePage: "home" });
+});
+
+app.get("/wws", (req, res) => {
+  res.render("services", { activePage: "home" });
+});
+
+app.get("/automotive", (req, res) => {
+  res.render("automotive");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
---
+++ .history/app_20230721152307.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.json()); // Parse JSON request bodies
+
+// Set the template engine
+app.set("view engine", "ejs");
+app.use("/assets", express.static("public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home", { activePage: "home" });
+});
+
+app.get("/wws", (req, res) => {
+  res.render("services", { activePage: "services" });
+});
+
+app.get("/automotive", (req, res) => {
+  res.render("automotive", { activePage: "services" });
+});
+
+app.get("/industrial", (req, res) => {
+  res.render("industrial", { activePage: "services" });
+});
+
+app.get("/industrial", (req, res) => {
+  res.render("industrial", { activePage: "services" });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
