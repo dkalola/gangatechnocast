ca1fab108c09ee20ca170511c18cfd4b5f6cc5ae
Divyanshu <divyanshukalola88@gmail.com>
2023-07-27 15:27:05 -0500

    removed history

--- .history/app_20230715120424.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.json()); // Parse JSON request bodies
-app.use(express.static("public")); // Serve static files from the 'public' directory
-
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
-});
