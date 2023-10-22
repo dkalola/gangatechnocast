dc2b3550a035d2e90bcc950742af74bcb45cdabc
Divyanshu <divyanshukalola88@gmail.com>
2023-10-22 11:57:20 -0500

    Test bug fix

--- Tests/main.test.js
+++ Tests/main.test.js
@@ -130,7 +130,7 @@ describe("Test routes", () => {
       "<title>Ganga Technocast - Contact</title>"
     );
     expect(response.text).toContain(
-      "<p>7QMM+QR2, Amin marg, Road, Amrut Park, </p> <p> Kotecha Nagar, Rajkot, Gujarat 360001, India</p>"
+      "<p>Survey No: 61/P3, Village: Bharudi, Tal: Gondal, </p> <p> <br> Rajkot, Gujarat 360311, India</p>"
     );
 
     expect(response.text).toContain(
